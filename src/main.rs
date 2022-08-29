5aba404794276a56b4e384c321327975a557b568
ericthelemur <owenconnors1@gmail.com>
2022-08-29 21:58:10 +0100

    Added basic install command endpoint

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ extern crate dotenv;
 
 use dotenv::dotenv;
 use rocket::State;
+use rocket::fs::TempFile;
+use rocket::serde::{Serialize, Deserialize};
+use rocket::form::Form;
 use entities::{*, prelude::*};
 use sea_orm::{Database, DatabaseConnection, EntityTrait};
 use rocket::serde::json::Json;
@@ -16,6 +19,33 @@ async fn list(db: &State<DatabaseConnection>) -> Json<Vec<game::Model>> {
     Json(Game::find().all(db).await.unwrap())
 }
 
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Commands {
+    install: String,
+    run: String,
+}
+
+#[get("/cmds/<id>")]
+async fn commands(db: &State<DatabaseConnection>, id: u32) -> Option<Json<Commands>> {
+    // TODO: Move to config and check these commands are actually correct lol
+    let db = db as &DatabaseConnection;
+    let install_dir = "~/gamingget";
+    let host = "localhost:8000";
+    if let Ok(Some(game)) = Game::find_by_id(id as i32).one(db).await {
+        let archive = &game.archive;
+        let url = host.to_owned() + "/games/" + archive;
+        let ext = ".tar.gz";
+        let output = if archive.ends_with(ext) { &archive[..archive.len() - ext.len()] } else { archive };
+        return Some(Json(Commands {
+            install: format!("cd \"{install_dir}\"; curl \"{url}\"; tar -xvf \"{archive}\" -o \"{output}\"; \"./{output}/setup.sh\""),
+            run: format!("cd \"{install_dir}\"; \"./{output}/run.sh\""),
+        }));
+    } else {
+        return None;
+    }
+}
+
 #[launch]
 async fn rocket() -> _ {
     dotenv().ok();
@@ -24,6 +54,6 @@ async fn rocket() -> _ {
 
     rocket::build()
         .manage(db)
-        .mount("/", routes![list])
+        .mount("/", routes![list, commands])
         .mount("/games", rocket::fs::FileServer::from("games/"))
 }
